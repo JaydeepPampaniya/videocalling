666e4e8a5aca16368037c0947c40d7de76238cd8
Jaydeep Pampaniya <jaydeeppampaniya77@gmail.com>
2023-08-13 15:08:49 +0530

    video calling web
    best video calling for free now

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (!location.hash) {
 const roomHash = location.hash.substring(1);
   
 // TODO: Replace with your own channel ID
-const drone = new ScaleDrone('l5MJ3u93YZK2N3vh');
+const drone = new ScaleDrone('ZpkiZ6DYW7c4ef0y');
 // Room name needs to be prefixed with 'observable-'
 const roomName = 'observable-' + roomHash;
 const configuration = {
